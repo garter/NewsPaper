d971ef19df2dd81ff5600a450eaa5537ef9eaeb0
garter <bayram88@live.ru>
2015-05-17 08:10:11 +0300

    Создание в библиотке класса config в это классе все конфигурационные данны

---
+++ lib/config_class.php
@@ -0,0 +1,15 @@
+<?php
+class Config{
+    
+    public $sitename = "newspaper";
+    public $address = "http://newspaper";
+    public $secret = "fornews";
+    public $host = "localhost";
+    public $dbname = "newsbase";
+    public $db_prefix = "news_";
+    public $db_user = "root";
+    public $db_pass = "";
+    public $admin_n = "Bayram";
+    public $admin_e = "bayram88@live.ru";
+    
+}
