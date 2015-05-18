2897e9f39ad0e776b75f12c8cc18c66e5f19bd9a
garter <bayram88@live.ru>
2015-05-18 05:56:15 +0300

    Создание в библиотке класса для работы с таблицой Articles

---
+++ lib/article_class.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'global_class.php';
+
+class Article extends GlobalClass{
+    
+    public function __construct( $db) {
+        parent::__construct("articles", $db);
+    }
+    
+    public function getAllSortDate(){
+        return $this->getAll("date", false);  
+    }
+    
+    public function getAllOnSectionID($section_id){
+        return $this->getAllOnField("section_id", $section_id, "date", false);
+    }    
+}
+
+
